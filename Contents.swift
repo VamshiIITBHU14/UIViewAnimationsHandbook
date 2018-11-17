a610aa504dec2e59036494888056ce77e2368d8f
Vamshi Krishna <guduru.vkrishna.civ10@itbhu.ac.in>
2018-11-17 21:46:21 +0530

    Init

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+import PlaygroundSupport
+
+
+class MainViewController : UIViewController{
+    var demoView = UIView(frame: CGRect(x: 0, y: 60, width: 120, height: 40))
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.blue
+        demoView.backgroundColor = UIColor.green
+        self.view.addSubview(demoView)
+        
+        title = "UIView Animations"
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        demoView.center.x = -self.view.bounds.width
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.5) {
+            self.demoView.center.x += 1.5*self.view.bounds.width
+            
+        }
+    }
+}
+
+let vc = MainViewController()
+let nav = UINavigationController(rootViewController: vc)
+PlaygroundPage.current.liveView = nav
+
